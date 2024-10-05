f58d9f41fde4e69158d67a30a98263add415b6c9
Ravindi Kumarasinghe <ravindikumarasinghe2@gmail.com>
2024-10-05 10:15:43 +0530

    Q7 to Q10

---
+++ src/main/java/com/ucsc/automation/theory/streamandlambda/Q7.java
@@ -0,0 +1,12 @@
+package com.ucsc.automation.theory.streamandlambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Q7 {
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(2,3,5,8,9,12);
+        List<Integer> sqList = integerList.stream().map(x-> x+x).toList();
+        System.out.println(sqList);
+    }
+}
